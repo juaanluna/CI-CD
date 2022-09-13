cb9815f88b8a57270256173efa71401d0711fc31
juaanluna <juaanluna@gmail.com>
2022-09-13 18:30:10 -0300

    adicionando novos testes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Teste CICD/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders description', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Esse projeto tem como objetivo a criação de um CICD/i);
   expect(linkElement).toBeInTheDocument();
 });
