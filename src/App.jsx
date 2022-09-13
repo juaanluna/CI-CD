6b62c43caac49d16cccd982619d54d68366649b4
juaanluna <juaanluna@gmail.com>
2022-09-13 19:09:23 -0300

    adding eslint

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        teste CICD
+      </h1>
+      <h5>
+        Esse projeto tem como objetivo a criaçaão de um CICD
+      </h5>
+    </div>
+  );
+}
+
+export default App;
